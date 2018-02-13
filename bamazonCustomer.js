8f38d70c254484dca4db377a79b8e29bfba60104
Cecily Grossmann <grssmnn2@gmail.com>
2018-02-13 13:40:19 -0600

    got inquirer running

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,3 +1,5 @@
+// var passwords = require('./passwords.js');
+// var password = new connection(passwords.userPass);
 // require inquirer npm to ask user for input
 var inquirer = require('inquirer');
 // require mysql to use mysql tables/database
@@ -6,7 +8,7 @@ var connection = mysql.createConnection({
   host     : 'localhost',
   port     :  3306,
   user     : 'root',
-  password : '',
+  password : "",
   database : "bamazonDB"
 });
  
@@ -16,13 +18,60 @@ connection.connect(function(err) {
     return;
   }
  
-  console.log('connected as id ' + connection.threadId);
+//   console.log('connected as id ' + connection.threadId);
+  afterConnection();
 });
 
 function afterConnection() {
     connection.query("SELECT * FROM products", function(err, res) {
       if (err) throw err;
-      console.log(res);
+      for(var i=0; i<res.length; i++){
+      console.log("Item ID: " + res[i].item_id + " Product: " + res[i].product_name + " Price: $" + res[i].price);
+      }
+      ask();
       connection.end();
     });
 };
+// ask user to place an order prompting for ID and number of items 
+function ask() {
+    // prompt user for guess and keep track of user's remaining guess
+    inquirer.prompt([
+      {
+        message: "Please enter the ID of the item you'd like to purchase",
+        name: "itemID",
+        type: "input",
+        validate: function (value) {
+          if (!value.match(/^[0-9]+$/)) {
+            return('Please only enter number values');
+            return false;
+          } return true;
+        }
+  
+      },
+      {
+          message: "Please enter the quantity of items you'd like to buy.",
+          name: "quantity",
+          type: "input",
+          validate: function (value) {
+            if (!value.match(/^[0-9]+$/)) {
+              return('Please only enter number values');
+              return false;
+            } return true;
+          }
+      }
+    ]).then(function (answers) {    
+      var itemID = +answers.itemID;
+      var requestedQuantity = +answers.quantity;
+     
+      if (this.quantity < requestedQuantity){
+          console.log("Sorry, there is insufficient stock to fill your order.");
+      }else{
+          console.log("Thank you for your order. Your total is " + (+this.price * requestedQuantity));
+          this.quantity = this.quantity-requestedQuantity;
+      }
+       
+ 
+    });
+  }
+
+ 
---
+++ passwords.js
@@ -0,0 +1,1 @@
+exports.userPass = process.env.password
