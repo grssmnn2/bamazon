f74ccd54ac92a62c647df3957d3f2072506b3211
Cecily Grossmann <grssmnn2@gmail.com>
2018-02-13 11:53:59 -0600

    setting up initial files

---
+++ bamazonCustomer.js
@@ -0,0 +1,18 @@
+// require inquirer npm to ask user for input
+var inquirer = require('inquirer');
+// require mysql to use mysql tables/database
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'example.org',
+  user     : 'bob',
+  password : 'secret'
+});
+ 
+connection.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+ 
+  console.log('connected as id ' + connection.threadId);
+});
